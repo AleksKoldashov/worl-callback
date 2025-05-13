bdbd0e88633c958152f2c59ae490d3d34af367e4
Aleksey Koldashov <123095853+AleksKoldashov@users.noreply.github.com>
2025-05-13 22:18:11 +0400

    add cors

--- netlify/functions/callback.js
+++ netlify/functions/callback.js
@@ -1,19 +1,37 @@
 exports.handler = async (event) => {
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST, OPTIONS",
+      },
+      body: JSON.stringify({}),
+    };
+  }
+
   if (event.httpMethod !== "POST") {
     return {
       statusCode: 405,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
       body: JSON.stringify({ error: "Only POST requests are allowed." }),
     };
   }
 
   try {
     const { name } = JSON.parse(event.body);
-
     const userName = name || "no name";
 
     return {
       statusCode: 200,
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
       body: JSON.stringify({
         message: `Thank you for your interest, ${userName}.`,
       }),
@@ -21,6 +39,9 @@ exports.handler = async (event) => {
   } catch (err) {
     return {
       statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
       body: JSON.stringify({ error: "Invalid JSON format." }),
     };
   }
